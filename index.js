62e57c3cc2933511a2cee626eb08cb13da8a4b30
Bayo <acadewole@gmail.com>
2022-07-20 15:48:13 -0500

    Adjust box widths

--- index.js
+++ index.js
@@ -1,19 +1,29 @@
-//Create the container div
-function createContainerDiv() {
+//create container
+function createContainer() {
   const container = document.createElement('div');
   container.classList.add('container');
   document.body.appendChild(container);
 }
-createContainerDiv();
+createContainer();
 
-//use for loop to create (16x16) grid items
-let gridSquares = 16 * 16;
+let numOfBoxes = 16 * 16;
 let number = 1;
 
-let divArray = new Array();
-for (i = 0; i < gridSquares; i++) {
-  divArray[i] = document.createElement('div');
-  divArray[i].textContent = `${number++}`;
-  const parentElement = document.querySelector('.container');
-  parentElement.appendChild(divArray[i]);
+for (i = 0; i < numOfBoxes; i++) {
+  const container = document.querySelector('.container');
+  const newDiv = document.createElement('div');
+  newDiv.textContent = number++;
+  container.appendChild(newDiv);
 }
+
+// //use for loop to create (16x16) grid items
+// let gridSquares = 16 * 16;
+// let number = 1;
+
+// let divArray = new Array();
+// for (i = 0; i < gridSquares; i++) {
+//   divArray[i] = document.createElement('div');
+//   divArray[i].textContent = number++;
+//   const parentElement = document.querySelector('.container');
+//   parentElement.appendChild(divArray[i]);
+// }
